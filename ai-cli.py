e822372ee23c041956a6da3c048d85ddd5395586
Andreas Evans <andr3as06@gmail.com>
2025-01-26 00:30:31 +0100

    Added a user profile directory

--- ai-cli.py
+++ ai-cli.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-import argparse
-import dotenv
 import os
 import sys
-import platform
-from ai import build_history, list_patterns, load_pattern, perform_request
+import ai
+
 
 def switch_input():
+    import platform
     if not sys.stdin.isatty():
         if platform.system() == "Windows":
             sys.stdin = open("CON:", "r")
@@ -15,10 +14,12 @@ def switch_input():
             sys.stdin.close()
             sys.stdin = os.fdopen(1)
 
+
 def get_input(prompt: str = "") -> str:
     if prompt:
         print(prompt, end="", flush=True)
     return sys.stdin.read()
+
 
 def print_completion(completion, is_stream: bool):
     output = ""
@@ -34,8 +35,19 @@ def print_completion(completion, is_stream: bool):
 
     return output
 
+
+def list_patterns():
+    patterns, error = ai.list_patterns()
+    if error is not None:
+        print(error, file=sys.stderr)
+        exit(1)
+        return
+    for pattern in patterns:
+        print(pattern)
+
+
 def main():
-    dotenv.load_dotenv(os.path.dirname(os.path.realpath(__file__)) + "/.env")
+    import argparse
 
     pattern = None
     temperature = 0.7
@@ -46,24 +58,25 @@ def main():
     is_chat = False
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--list-patterns", action='store_true', help="List available patterns")
-    parser.add_argument("-t", "--temperature", type=float, help="The temperature to use")
-    parser.add_argument("-m", "--model", type=str, help="The model to use")
-    parser.add_argument("-p", "--prompt", type=str, help="Define a custom system prompt")
-    parser.add_argument("-u", "--user", type=str, help="Define a custom user input")
-    parser.add_argument("-c", "--chat", action='store_true', help="Enter chat mode after creating the initial output")
-    parser.add_argument("PATTERN", type=str, help="The pattern to use", nargs='?')
+    parser.add_argument("-l", "--list-patterns",
+                        action='store_true', help="List available patterns")
+    parser.add_argument("-t", "--temperature",
+                        type=float, help="The temperature to use")
+    parser.add_argument("-m", "--model",
+                        type=str, help="The model to use")
+    parser.add_argument("-p", "--prompt",
+                        type=str, help="Define a custom system prompt")
+    parser.add_argument("-u", "--user",
+                        type=str, help="Define a custom user input")
+    parser.add_argument("-c", "--chat",
+                        action='store_true', help="Enter chat mode")
+    parser.add_argument("PATTERN",
+                        type=str, help="The pattern to use", nargs='?')
 
     args = parser.parse_args()
 
     if args.list_patterns:
-        patterns, error = list_patterns()
-        if error is not None:
-            print(error, file=sys.stderr)
-            exit(1)
-            return
-        for pattern in patterns:
-            print(pattern)
+        list_patterns()
         exit(0)
 
     if args.PATTERN is not None:
@@ -80,7 +93,7 @@ def main():
 
     if args.user is not None:
         user_input = args.user
-    
+
     if args.chat is not None:
         is_chat = args.chat
 
@@ -91,24 +104,28 @@ def main():
     if not sys.stdin.isatty():
         user_input += sys.stdin.read()
 
+    import dotenv
+    dotenv.load_dotenv(os.path.dirname(os.path.realpath(__file__)) + "/.env")
+
     try:
         history = []
         if pattern is not None:
             # FIXME: This overrides the user defined system prompt
-            system_input, user_input, error = load_pattern(pattern, user_input)
+            system_input, user_input, error = ai.load_pattern(pattern, user_input)
             if error is not None:
                 print(error, file=sys.stderr)
                 exit(1)
         elif system_input != "" or is_chat:
-            pass # Nothing to do
+            pass  # Nothing to do
         else:
             parser.print_help()
             exit(2)
 
         if system_input != "":
-            build_history(history, system_input, user_input)
-            
-            completion, error = perform_request(history, is_stream, temperature, model)
+            ai.build_history(history, system_input, user_input)
+
+            completion, error = ai.perform_request(history, is_stream,
+                                                temperature, model)
 
             if error is not None:
                 print(error, file=sys.stderr)
@@ -124,7 +141,8 @@ def main():
 
                 history.append({"role": "user", "content": stdin})
 
-                completion, error = perform_request(history, is_stream, temperature, model)
+                completion, error = ai.perform_request(history, is_stream,
+                                                    temperature, model)
                 if error is not None:
                     print(error, file=sys.stderr)
                     exit(1)
@@ -135,5 +153,7 @@ def main():
         print("User interrupted execution", file=sys.stderr)
         exit(3)
 
+
 if __name__ == "__main__":
     main()
+
--- ai.py
+++ ai.py
@@ -1,11 +1,11 @@
-import sys
 from typing import Any, Tuple
-from openai import AuthenticationError, OpenAI, RateLimitError
 import os
 
 _client = None
 
+
 def get_client() -> Tuple[Any, str]:
+    from openai import OpenAI
     global _client
 
     try:
@@ -16,24 +16,76 @@ def get_client() -> Tuple[Any, str]:
 
         _client = OpenAI(base_url=api_url, api_key=api_key)
     except Exception as e:
-        return None, f"Failed to create OpenAI client: {e}" 
+        return None, f"Failed to create OpenAI client: {e}"
 
     return _client, None
 
+
+def get_user_patterns_path() -> str:
+    user_patterns_path = None
+
+    # TODO: Handle windows systems
+
+    if os.getenv('XDG_CONFIG_HOME') is not None:
+        user_patterns_path = os.getenv('XDG_CONFIG_HOME') + "/ai-cli/patterns"
+    elif os.getenv('HOME'):
+        user_patterns_path = os.getenv('HOME') + "/.config/ai-cli/patterns"
+
+    return user_patterns_path
+
+
+def get_builtin_patterns_path() -> str:
+    return os.path.dirname(os.path.realpath(__file__)) + "/patterns"
+
+
+def list_pattern_from_directory(path: str):
+    if not os.path.exists(path):
+        return []
+
+    return os.listdir(path)
+
+
+def find_pattern_path(pattern_name: str) -> str:
+    user_patterns_path = get_user_patterns_path()
+
+    if user_patterns_path is not None:
+        patterns = list_pattern_from_directory(user_patterns_path)
+        if pattern_name in patterns:
+            return user_patterns_path + "/" + pattern_name
+
+    builtin_patterns_path = get_builtin_patterns_path()
+
+    if builtin_patterns_path is not None:
+        patterns = list_pattern_from_directory(builtin_patterns_path)
+        if pattern_name in patterns:
+            return builtin_patterns_path + "/" + pattern_name
+
+    return None
+
+
 def list_patterns():
-    pattern_path = os.path.dirname(os.path.realpath(__file__)) + "/patterns"
-    if not os.path.exists(pattern_path):
-        return None, "Patterns directory not found"
+    patterns = []
+    patterns += list_pattern_from_directory(get_builtin_patterns_path())
 
-    return os.listdir(pattern_path), None
+    user_patterns_path = get_user_patterns_path()
+    if user_patterns_path is not None:
+        patterns += list_pattern_from_directory(user_patterns_path)
 
-def build_history(history: list, system_input: str, user_input: str = "", stdin: str = ""):
+    return patterns, None
+
+
+def build_history(
+        history: list,
+        system_input: str,
+        user_input: str = "",
+        stdin: str = ""):
     history.append({"role": "system", "content": system_input})
     if user_input + stdin != "":
         history.append({"role": "user", "content": user_input + stdin})
 
+
 def load_pattern(pattern: str, user_input: str = ""):
-    pattern_path = os.path.dirname(os.path.realpath(__file__)) + "/patterns/" + pattern
+    pattern_path = find_pattern_path(pattern) 
     if not os.path.exists(pattern_path):
         return None, None, f"Pattern '{pattern}' not found"
 
@@ -47,7 +99,13 @@ def load_pattern(pattern: str, user_input: str = ""):
 
     return system_input, user_input, None
 
-def perform_request(history: list, is_stream: bool, temperature: float = 0.7, model: str = None):
+
+def perform_request(
+        history: list,
+        is_stream: bool,
+        temperature: float = 0.7,
+        model: str = None):
+    from openai import AuthenticationError, RateLimitError
     client, error = get_client()
     if error:
         return None, error
@@ -62,12 +120,13 @@ def perform_request(history: list, is_stream: bool, temperature: float = 0.7, mo
             temperature=temperature,
             stream=is_stream,
         )
-        
+
         return completion, None
     except AuthenticationError as e:
         return None, "Failed to authenticate to server: " + e.body["message"]
     except RateLimitError as e:
         return None, "API rate limit exceeded: " + e.body["message"]
+
 
 def extract_completion(completion, is_stream: bool):
     output = ""
